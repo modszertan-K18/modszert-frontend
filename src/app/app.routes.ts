c9c15567420eae9b40d73a88a69d5343ac49afd7
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-11-16 15:30:23 +0100

    Call the login and register endpoints

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
+import {RegisterComponent} from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "login", component: SignInComponent },
+  { path: "register", component: RegisterComponent },
+];
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
-  standalone: false,
+  standalone: true,
   templateUrl: './sign-in.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    const loginData = { username: this.username, password: this.password };
+
+    this.http.post(`${environment.backendBaseUrl}/auth/login`, loginData).subscribe({
+      next: (response: any) => {
+        console.log('Login successful', response);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      }
+    });
+  }
 }

e38852a11c655ba21723c7ebdf536108a8edf0fd
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-11-24 14:40:46 +0100

    Add create product page and init home with nav button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {SignInComponent} from './pages/sign-in/sign-in.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {CreateProductComponent} from './pages/create-product/create-product.component';
+import {HomeComponent} from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: "login", component: SignInComponent },
   { path: "register", component: RegisterComponent },
+  { path: "create", component: CreateProductComponent },
+  { path: "", component: HomeComponent }
 ];
